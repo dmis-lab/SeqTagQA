a56ac00b6e755ecab7f869258ab40835485b4945
Wonjin Yoon <13089179+wonjininfo@users.noreply.github.com>
2022-04-21 17:59:17 +0900

    Update train_list_example.sh

--- train_list_example.sh
+++ train_list_example.sh
@@ -23,6 +23,7 @@ export OUTPUT_DIR=<Output path>/20220130-BioASQ-list${VERSION}b_${LM}-${METHOD}_
 echo $OUTPUT_DIR
 mkdir $OUTPUT_DIR
 
+# To train
 python run_eqa.py --model_struct=${METHOD} \
  --do_train=true --do_eval=true --do_predict=true \
  --model_name_or_path=$LANGMODEL_DIR \
@@ -33,16 +34,26 @@ python run_eqa.py --model_struct=${METHOD} \
  --num_train_epochs=${MAX_EPOCH} \
  --save_steps=4000
 
-# To Detokenize the output
+# To predict
 export CKPT=84000
 export SUB_OUTPUT_DIR=${OUTPUT_DIR}/checkpoint-${CKPT}
+python run_eqa.py --model_struct=${METHOD} \
+  --do_train=false --do_eval=false --do_predict=true \
+  --model_name_or_path=$SUB_OUTPUT_DIR \
+  --per_device_train_batch_size=18 --per_device_eval_batch_size=32 --max_seq_length=512 \
+  --test_file=${DATA_DIR}/test.tsv \
+  --output_dir=$SUB_OUTPUT_DIR --save_total_limit=50 \
+  --learning_rate=${LEARN_RATE} --seed=${SEED} \
+  --save_steps=4000
+
+# To Detokenize the output
 python detokenize-list.py \
   --test_path=${DATA_DIR}/test.tsv \
   --predictions_path=${SUB_OUTPUT_DIR}/predictions.txt \
   --original_test_path=${DATA_DIR}/original_test.json \
   --output_dir=${SUB_OUTPUT_DIR}
 
-# Example code for official BioASQ evaluation codes
+# Example script of using official BioASQ evaluation library
 #export EVAL_LIB=<Cloned official eval code path>/Evaluation-Measures
 #java -Xmx10G -cp $CLASSPATH:$EVAL_LIB/flat/BioASQEvaluation/dist/BioASQEvaluation.jar evaluation.EvaluatorTask1b -phaseB -e 5 \
 #  <Path to DATA>/DATA/BioASQ-golden/${VERSION}B_total_golden.json ${SUB_OUTPUT_DIR}/NER_result_BioASQ.json >> $OUTPUT_DIR/total_BioASQ_eval.log
